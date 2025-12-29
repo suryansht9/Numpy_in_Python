1018082eb5e058198137a5cc69e651e5cda4f7d5
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-12-29 10:15:04 +0530

    Create statistical_function.py

---
+++ statistical_function.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Mean:", np.mean(arr))
+print("Median:", np.median(arr))
+print("Standard Deviation:", np.std(arr))
+print("Variance:", np.var(arr))
